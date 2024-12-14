28548db8c669cfca9432f7e2a578359e4bad654b
dougc95 <dougcr95@gmail.com>
2024-12-14 11:30:13 -0400

    give styles more structure

---
+++ src/styles/palette.js
@@ -0,0 +1,42 @@
+const palette = {
+  primary: {
+    light: "#2f7059",
+    main: "#2f7059",
+    dark: "#90ee90",
+  },
+  secondary: {
+    light: "#2e2e2e",
+    main: "#2e2e2e",
+    dark: "#d3d3d3",
+  },
+  menstrual: {
+    light: "#fecaca",
+    main: "#dc2626",
+    dark: "#7f1d1d",
+  },
+  follicular: {
+    light: "#dcfce7",
+    main: "#4ade80",
+    dark: "#166534",
+  },
+  ovulation: {
+    light: "#bfdbfe",
+    main: "#3b82f6",
+    dark: "#1d4ed8",
+  },
+  luteal: {
+    light: "#fef9c3",
+    main: "#facc15",
+    dark: "#a16207",
+  },
+  background: {
+    light: "#ffffff",
+    dark: "#000000",
+  },
+  text: {
+    light: "#000000",
+    dark: "#ffffff",
+  },
+};
+
+export default palette;
